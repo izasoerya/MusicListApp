2da3df3f46401e0854040f4738ef07cedd26f4ef
izasoerya <ihza.surya@gmail.com>
2023-08-16 23:41:19 +0700

    change dsn

--- database/db.go
+++ database/db.go
@@ -11,12 +11,12 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() error {
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+						os.Getenv("DB_USER"),
+						os.Getenv("DB_PASSWORD"),
+						os.Getenv("DB_HOST"),
+						os.Getenv("DB_PORT"),
+						os.Getenv("DB_NAME"))
 	
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
